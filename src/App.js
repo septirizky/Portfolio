b9b35a2c6db6dc9f51f3ecd230d2d5b21c53358c
septi <septirizky79@gmail.com>
2025-04-21 11:14:02 +0700

    update

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import { Route, Routes } from 'react-router-dom'
+import Layout from './components/Layout'
+import Home from './components/Home'
+import About from './components/About'
+import Tech from './components/Tech'
+import Portfolio from './components/Portfolio'
+import Contact from './components/Contact'
+import './App.scss'
+
+function App() {
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/tech" element={<Tech />} />
+          <Route path="/portfolio" element={<Portfolio />} />
+          <Route path="/contact" element={<Contact />} />
+        </Route>
+      </Routes>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Portfolio/index.js
@@ -0,0 +1,112 @@
+import React, { useEffect, useState } from 'react'
+import Loader from 'react-loaders'
+import AnimatedLetters from '../AnimatedLetters'
+import './index.scss'
+import S1 from '../../assets/images/reservasi.png'
+import S2 from '../../assets/images/dashboard.png'
+import S3 from '../../assets/images/Aditi.png'
+import S4 from '../../assets/images/Realta.png'
+import S5 from '../../assets/images/Quote.png'
+import S6 from '../../assets/images/MitraMix.png'
+
+const Portfolio = () => {
+  const [letterClass, setLetterClass] = useState('text-animate')
+  const nameArray = ['P', 'o', 'r', 't', 'f', 'o', 'l', 'i', 'o']
+
+  useEffect(() => {
+    return setTimeout(() => {
+      setLetterClass('text-animate-hover')
+    }, 3000)
+  }, [])
+
+  return (
+    <>
+      <div className="container portfolio-page">
+        <h1 className="text">
+          <AnimatedLetters
+            letterClass={letterClass}
+            strArray={nameArray}
+            idx={15}
+          />
+        </h1>
+        <div className="flexbox-portfolio">
+          <div className="content-portfolio">
+            <img src={S1} alt="Logo1" />
+            <div className="layer">
+              <h2>reservasi Bandar Djakarta</h2>
+              <p>Reservation website for Bandar Djakarta Restaurant</p>
+              <p>#Flask #ReactJS #Redux #Tailwind</p>
+              <a href="https://reservasi.bandjak.com/">
+                <i className="bx bx-lunk-external"></i>
+              </a>
+            </div>
+          </div>
+          <div className="content-portfolio">
+            <img src={S2} alt="Logo1" />
+            <div className="layer">
+              <h2>Dasboard Bandar Djakarta</h2>
+              <p>Dasboard website for Management Bandar Djakarta</p>
+              <p>#Flask #ReactJS #Redux #Tailwind</p>
+              <a href="#">
+                <i className="bx bx-lunk-external"></i>
+              </a>
+            </div>
+          </div>
+          <div className="content-portfolio">
+            <img src={S3} alt="Logo1" />
+            <div className="layer">
+              <h2>Aditi Engineering</h2>
+              <p>Company profile website for business truck modification</p>
+              <p>#ReactJS #Typescript #NextJs #Tailwind #Daisyui</p>
+              <a href="https://aditi-engineering.vercel.app/">
+                <i className="bx bx-lunk-external"></i>
+              </a>
+            </div>
+          </div>
+          <div className="content-portfolio">
+            <img src={S4} alt="Logo2" />
+            <div className="layer">
+              <h2>Realta Hotel</h2>
+              <p>
+                This website is used to assist realta hotels in the room booking
+                process
+              </p>
+              <p>#Postgresql #ExpressJs #Sequelize #Multer #React #Bootstrap</p>
+              <a href="#">
+                <i className="bx bx-lunk-external"></i>
+              </a>
+            </div>
+          </div>
+          <div className="content-portfolio">
+            <img src={S5} alt="Logo3" />
+            <div className="layer">
+              <h2>Random Quote Generator</h2>
+              <p>
+                Our Random Quote Generator is a simple yet powerful tool
+                designed to inspire, motivate, and entertain.
+              </p>
+              <p>#Third-party APIs #ReactJs #MaterialUI</p>
+              <a href="https://septi-random-quote.vercel.app/">
+                <i className="bx bx-lunk-external"></i>
+              </a>
+            </div>
+          </div>
+          <div className="content-portfolio">
+            <img src={S6} alt="Logo4" />
+            <div className="layer">
+              <h2>MITRAMIX</h2>
+              <p>Company profile website for ready mix supplier.</p>
+              <p>#ReactJS #Typescript #NextJs #Tailwind #Daisyui</p>
+              <a href="https://septi-random-quote.vercel.app/">
+                <i className="bx bx-lunk-external"></i>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <Loader type="pacman" />
+    </>
+  )
+}
+
+export default Portfolio
